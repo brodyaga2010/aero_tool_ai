4a7b56acf801da98936bf70af97b93bc184abc4e
cyc <tyver9@gmail.com>
2025-10-03 00:28:25 +0500

    Добавил поле цвета в результат и сделал цветными рамки

--- api/app/models/yolo_detector.py
+++ api/app/models/yolo_detector.py
@@ -22,6 +22,21 @@ class YOLODetector:
         for model in self.models:
             model.conf = confidence_threshold
 
+        # Цвета для классов
+        self.class_colors = {
+            "screwdriver_1": "blue",
+            "screwdriver_2": "green",
+            "Offset_Phillips": "orange",
+            "Side_cutters": "purple",
+            "Shernica": "pink",
+            "Safety_pliers": "cyan",
+            "Pliers": "yellow",
+            "Rotary_wheel": "brown",
+            "Open_end_wrench": "lime",
+            "Oil_can_opener": "magenta",
+            "Adjustable_wrench": "teal",
+        }
+
     def update_confidence_threshold(self, threshold: float):
         """Обновление порога уверенности"""
         self.confidence_threshold = threshold
@@ -69,7 +84,17 @@ class YOLODetector:
             if cls not in final_detections or det["confidence"] > final_detections[cls]["confidence"]:
                 final_detections[cls] = det
 
-        detections = list(final_detections.values())
+        detections = []
+        for det in final_detections.values():
+            cls = det["class"]
+            color = self.class_colors.get(cls, "red")
+
+            detections.append({
+                "bbox": det["bbox"],
+                "confidence": det["confidence"],
+                "class": cls,
+                "color": color
+            })
 
         # Сохраняем изображение с финальными боксами
         output_dir = Path("static/results")
@@ -82,10 +107,15 @@ class YOLODetector:
 
         img_with_boxes = image.copy()
         draw = ImageDraw.Draw(img_with_boxes)
+
         for det in detections:
             x1, y1, x2, y2 = det["bbox"]
-            draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
-            draw.text((x1, y1 - 10), f"{det['class']} {det['confidence']:.2f}", fill="red")
+            cls = det["class"]
+
+            color = self.class_colors.get(cls, "red")
+
+            # Рисуем рамку
+            draw.rectangle([x1, y1, x2, y2], outline=color, width=15)
 
         img_with_boxes.save(output_path)
 
